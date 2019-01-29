80cbd3df22a02c2f6e2802710cffa2b142e424cf
Christer Olsen <colsen1991@gmail.com>
2019-01-29 12:16:01 +0100

    Cleaned up a bit. Removed eslint. Updated text. Fixed a few styling errors.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hero">
+  <div class="hero is-medium">
     <div class="hero-body">
       <div class="container">
         <div class="columns">
@@ -18,22 +18,22 @@
               I’m a Norwegian self-employed web developer, beer enthusiast, and traveller.
             </p>
             <p>
-              I have a <span class="has-text-info">bachelor’s degree</span> in software engineering, as well as 2 years of
-              experience in the field. VINDIR is the name of my company, and the website can be found <a
+              I have a <span class="has-text-info">bachelor’s degree</span> in software engineering, as well as 3 years of
+              experience in the field. Vindir is the name of my company, and the website can be found <a
               href="https://www.vindir.no" target="_blank" rel="noopener nofollow">here</a> (in Norwegian).
             </p>
             <p>
-              Technologies I've worked with includes <span class="has-text-info">React</span>, Backbone.js, <span
+              Technologies and tools that I've worked with includes <span class="has-text-info">React</span>, Backbone.js, <span
               class="has-text-info">Vue.js</span> w/Nuxt.js, jQuery, <span class="has-text-info">Node.js</span>
               w/Express, <span class="has-text-info">webpack</span>, Babel, npm, Java, document and relational <span
               class="has-text-info">databases</span>. Git, HTML, CSS, and vanilla JavaScript goes without saying, but I'll
               mention it anyway.
             </p>
             <p>
-              Finished personal projects consists of this website, as well as <a href="https://www.beeroclock.site"
-                                                                                 target="_blank" rel="noopener nofollow">The
-              Beer O’Clock Clock</a>.
-              But my GitHub profile is home to plenty of half-finished and dead projects.
+              Finished personal projects consists of this website,
+              as well as <a href="https://www.beeroclock.site" target="_blank" rel="noopener nofollow">The Beer O’Clock Clock</a>.
+              My GitHub profile is also home to plenty of projects in various states. Lastly I have a good handful of closed source projects
+              connected to my company, and the link to each one can be found <a href="https://www.vindir.no/referanser" target="_blank" rel="noopener, nofollow">here</a>.
             </p>
             <p>
               Sounds interesting? Or just feeling lonely? Anyway; let's talk! Contact info in the footer!
@@ -52,3 +52,11 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
