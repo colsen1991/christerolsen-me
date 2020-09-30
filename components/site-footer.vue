f9b078cb8b9c438f03aa745dbafdd1224a5d571c
Christer Olsen <colsen1991@gmail.com>
2020-09-30 11:07:14 +0200

    Small updates to my resume and contact info.

--- components/site-footer.vue
+++ components/site-footer.vue
@@ -4,10 +4,8 @@
     <div class="columns">
       <hr class="is-hidden-tablet">
       <div class="column has-text-centered">
-        <a href="https://twitter.com/colsen1991" target="_blank" rel="noopener nofollow">twitter</a>&nbsp;
-        <a href="https://www.facebook.com/colsen1991" target="_blank" rel="noopener nofollow">facebook</a>&nbsp;
-        <a href="https://www.linkedin.com/in/colsen1991" target="_blank" rel="noopener nofollow">linkedin</a><br>
         <a href="https://github.com/colsen1991" target="_blank" rel="noopener nofollow">github</a>&nbsp;
+        <a href="https://www.linkedin.com/in/colsen1991" target="_blank" rel="noopener nofollow">linkedin</a><br>
         <a href="mailto:colsen1991@gmail.com">email</a>
       </div>
 
--- pages/index.vue
+++ pages/index.vue
@@ -21,13 +21,13 @@
               I have a <span class="has-text-info">bachelor’s degree</span> in software engineering, as well as {{new Date().getFullYear() - 2014}} years of
               experience in the field. Vindir is the name of my company, and the website can be found <a href="https://www.vindir.no" target="_blank" rel="noopener nofollow">here</a> (in Norwegian)
               I'm also a part-time employee at <a href="https://www.companycast.no" target="_blank" rel="noopener nofollow">CompanyCast</a>.
+              Lastly, from 2019 to 2020 I had a little side-venture called Chatrobot AS. But life and a change in Facebook policies happened, so...
             </p>
             <p>
               Technologies and tools that I've worked with includes <span class="has-text-info">React</span>, Backbone.js, <span
               class="has-text-info">Vue.js</span> w/Nuxt.js, jQuery, <span class="has-text-info">Node.js</span>
-              w/Express, <span class="has-text-info">webpack</span>, Babel, npm, Java, document and relational <span
-              class="has-text-info">databases</span>, Botkit.ai, and several others.
-              Git, HTML, CSS, and vanilla JavaScript goes without saying, but there you go.
+              w/Express, npm and its ecosystem, Java, document and relational <span class="has-text-info">databases</span>, Botkit.ai, C#, Php, and several others.
+              Git, HTML, CSS, and vanilla JavaScript should go without saying, but there you go :)
             </p>
             <p>
               Finished personal projects consists of this website, as well as <a href="https://www.beeroclock.site" target="_blank" rel="noopener nofollow">The Beer O’Clock Clock</a>.
