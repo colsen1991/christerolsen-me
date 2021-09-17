a6287884a28177d4546741ebbd61e3bff7014875
Christer Olsen <colsen1991@gmail.com>
2021-09-17 15:03:15 +0200

    Bump nvm, deps and fixed a bit of text.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const isProduction = process.env.TARGET_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   target: 'static',
@@ -6,13 +6,13 @@ module.exports = {
     fallback: true
   },
   css: [
-    {src: './assets/style/index.scss', lang: 'sass'}
+    { src: './assets/style/index.scss', lang: 'sass' }
   ],
   loading: {
     color: '#48E826'
   },
   modules: isProduction
-    ? ['@nuxtjs/sitemap', ['@nuxtjs/google-analytics', {id: 'UA-107229265-2'}]]
+    ? ['@nuxtjs/sitemap', ['@nuxtjs/google-analytics', { id: 'UA-107229265-2' }]]
     : undefined,
   build: {
     extractCSS: isProduction
@@ -22,22 +22,22 @@ module.exports = {
     './plugins/lazyload'
   ],
   head: {
-    htmlAttrs: {lang: 'en'},
+    htmlAttrs: { lang: 'en' },
     title: 'Christer Olsen: Web Developer',
     link: [
-      {rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'author', href: '/humans.txt'}
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'author', href: '/humans.txt' }
     ],
     meta: [
-      {hid: 'charset', charset: 'utf-8'},
-      {hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'theme-color', content: '#FB5607'},
-      {hid: 'og:title', property: 'og:title', content: 'Christer Olsen: Web Developer'},
-      {hid: 'description', name: 'description', content: 'Homepage of Christer Olsen, a Norwegian web developer'},
-      {hid: 'og:description', property: 'og:description', content: 'Homepage of Christer Olsen, a Norwegian web developer'},
-      {hid: 'og:image', property: 'og:image', content: 'https://www.christerolsen.me/img/me-lg.png'},
-      {hid: 'og:site_name', property: 'og:site_name', content: 'Christer Olsen: Web Developer'},
-      {name: 'robots', content: 'index, follow'}
+      { hid: 'charset', charset: 'utf-8' },
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#FB5607' },
+      { hid: 'og:title', property: 'og:title', content: 'Christer Olsen: Web Developer' },
+      { hid: 'description', name: 'description', content: 'Homepage of Christer Olsen, a Norwegian web developer' },
+      { hid: 'og:description', property: 'og:description', content: 'Homepage of Christer Olsen, a Norwegian web developer' },
+      { hid: 'og:image', property: 'og:image', content: 'https://www.christerolsen.me/img/me-lg.png' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Christer Olsen: Web Developer' },
+      { name: 'robots', content: 'index, follow' }
     ]
   },
   sitemap: {
--- pages/index.vue
+++ pages/index.vue
@@ -5,38 +5,18 @@
         <div class="columns">
           <div id="img-intro" class="column is-half is-one-third-fullhd has-text-centered content">
             <figure class="image is-max-256x256-fullhd is-max-384x384">
-              <img v-lazy="'/img/me.png'" alt="Portrait of me - Christer Olsen" title="The developer himself">
+              <img v-lazy="'/img/me.png'" alt="Portrait of me - Christer Olsen" title="The developer himself" />
             </figure>
             <h1 class="title is-marginless">Chris Olsen</h1>
             <h2 class="title is-marginless">Web Developer</h2>
           </div>
           <div id="story" class="column content">
-            <p>
-              As mentioned; the name's Chris!
-            </p>
-            <p>
-              I’m a Norwegian self-employed web developer, beer enthusiast, and traveller (from time to time).
-            </p>
-            <p>
-              I have a <span class="has-text-info">bachelor’s degree</span> in software engineering, as well as {{new Date().getFullYear() - 2014}} years of
-              experience in the field. Vindir is the name of my company, and the website can be found <a href="https://www.vindir.no" target="_blank" rel="noopener nofollow">here</a> (in Norwegian)
-              I'm also a part-time employee at <a href="https://www.companycast.no" target="_blank" rel="noopener nofollow">CompanyCast</a>.
-              Lastly, from 2019 to 2020 I had a little side-venture called Chatrobot AS. But life and a change in Facebook policies happened, so...
-            </p>
-            <p>
-              Technologies and tools that I've worked with includes <span class="has-text-info">React</span>, Backbone.js, <span
-              class="has-text-info">Vue.js</span> w/Nuxt.js, jQuery, <span class="has-text-info">Node.js</span>
-              w/Express, npm and its ecosystem, Java, document and relational <span class="has-text-info">databases</span>, Botkit.ai, C#, Php, and several others.
-              Git, HTML, CSS, and vanilla JavaScript should go without saying, but there you go :)
-            </p>
-            <p>
-              Finished personal projects consists of this website, as well as <a href="https://www.beeroclock.site" target="_blank" rel="noopener nofollow">The Beer O’Clock Clock</a>.
-              My GitHub profile is also home to plenty of projects in various states. Lastly I have a good handful of closed source projects
-              connected to my company, and the link to each one can be found <a href="https://www.vindir.no/referanser" target="_blank" rel="noopener, nofollow">here</a>.
-            </p>
-            <p>
-              Sounds interesting? Or just feeling lonely? Anyway; let's talk! Contact info in the footer!
-            </p>
+            <p>As mentioned; the name's Chris!</p>
+            <p>I’m a Norwegian self-employed web developer, beer enthusiast, and traveller (from time to time).</p>
+            <p>I have a <span class="has-text-info">bachelor’s degree</span> in software engineering, as well as {{ new Date().getFullYear() - 2014 }} years of experience in the field. Vindir is the name of my company, and the website can be found <a href="https://www.vindir.no" target="_blank" rel="noopener nofollow">here</a> (in Norwegian) I'm also a employee over at <a href="https://www.companycast.no" target="_blank" rel="noopener nofollow">CompanyCast</a>. Lastly, from 2019 to 2020 I had a little side-venture called Chatrobot AS. But life and a change in Facebook policies happened, so...</p>
+            <p>Technologies and tools that I've worked with includes <span class="has-text-info">React</span>, <span class="has-text-info">Vue.js</span> w/Nuxt.js, Backbone.js, jQuery, <span class="has-text-info">Node.js</span> w/Express, npm and its ecosystem, Java, document and relational <span class="has-text-info">databases</span>, Botkit.ai, C#, Php, and several others. Git, HTML, CSS, and JavaScript - should go without saying, but there you go :)</p>
+            <p>Finished personal projects consists of this website, as well as <a href="https://www.beeroclock.site" target="_blank" rel="noopener nofollow">The Beer O’Clock Clock</a>. My GitHub profile is also home to plenty of projects in various states. Lastly I have a good handful of closed source projects connected to my company, and the link to each one can be found <a href="https://www.vindir.no/referanser" target="_blank" rel="noopener, nofollow">here</a>.</p>
+            <p>Sounds interesting? Or just feeling lonely? Anyway; let's talk! Contact info in the footer!</p>
           </div>
         </div>
       </div>
@@ -47,7 +27,7 @@
 <script>
   export default {
     head: {
-      link: [{hid: 'canonical', rel: 'canonical', href: 'https://www.christerolsen.me'}]
+      link: [{ hid: 'canonical', rel: 'canonical', href: 'https://www.christerolsen.me' }]
     }
   }
 </script>
